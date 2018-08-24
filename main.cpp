f6ac3039d116314251150530d340bab90caf05ed
ASHOK-SINGH <37559719+XLAR-8@users.noreply.github.com>
2018-08-24 14:12:51 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "./header/pro.dec"
+
+int main ()
+{	
+   system("clear");
+	start:
+	kill = NULL;
+	int cnt = 1;
+	while(cnt){
+		if(kill == NULL)
+			cnt = main_menu();
+		else
+			{ usermenu:
+				cnt = user_menu();
+	//goto usermenu; }
+
+}
+}
+	goto start;	footer();
+	cout<<"\nHave a nice f day";
+	return 0;
+}
